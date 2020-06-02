b4246ee859a9cf95811431948e00229685d3975c
מתניה ילינק <matanya.je@hotmail.com>
2020-06-02 18:05:30 +0300

    fghfgh

--- routes/albums.js
+++ routes/albums.js
@@ -36,6 +36,8 @@ module.exports = {
     //READ - GET http://localhost:3001/albums/{albumId} 
     get_albums: function(req, res) {
         readFile(data => {
+            if (req.params.id == undefined)
+                return res.send("There is no album with ID ''.");
             var values = {};
             values = JSON.parse(data);
             var id = req.params.id;
@@ -51,6 +53,18 @@ module.exports = {
             var id = req.params.id;
             if (values[id] == undefined)
                 return res.send("Error.")
+            if (req.body.name == undefined || req.body.name.trim() == "") {
+                res.send("Wrong picture name.");
+                return;
+            }
+            if (req.body.photographer == undefined || req.body.photographer.trim() == "") {
+                res.send("Wrong photographer.");
+                return;
+            }
+            if (!isValidURL(req.body.link)) {
+                res.send("Url must be valid.");
+                return;
+            }
             var album = values[id]
             var pictures = album["pictures"];
             var i = 1;
@@ -73,7 +87,7 @@ module.exports = {
             values = JSON.parse(data);
             var id = req.params.id;
             if (values[id] == undefined)
-                return res.send("Error.")
+                return res.send("Error.");
 
             delete values[id];
 
@@ -86,10 +100,18 @@ module.exports = {
     // CREATE - POST http://localhost:3001/albums
     create_album: function(req, res) {
         readFile(data => {
-            if (req.body.name == undefined)
+            if (req.body.name == undefined) {
                 res.send("I didn't get the name.");
-            if (req.body.type == undefined)
+                return;
+            }
+            if (req.body.type == undefined) {
                 res.send("I didn't get the type.");
+                return;
+            }
+            if (req.body.type != "People" && req.body.type != "Nature") {
+                res.send("Type is not People or Nature.");
+                return;
+            }
             var values = {};
             values = JSON.parse(data);
             var i = 0;
@@ -97,6 +119,10 @@ module.exports = {
                 i++;
             values[i] = req.body;
             values[i].id = i + "";
+            if (req.body.pictures != {}) {
+                var album = values[i];
+                album["pictures"] = {};
+            }
 
             writeFile(JSON.stringify(values, null, 2), () => {
                 res.status(200).send('New album added.');
@@ -104,3 +130,16 @@ module.exports = {
         });
     }
 };
+
+function isValidURL(urlLink) {
+    var pattern = new RegExp(
+        "^(https?:\\/\\/)?" + // protocol
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]* ) *" + // port and path
+        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "(\\#[-a-z\\d_]*)?$",
+        "i"
+    );
+    return !!pattern.test(urlLink);
+}
